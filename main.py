b712730708452e35f195d88c313f67102aff6e51
João Nery <jvanery@gmail.com>
2026-07-20 22:34:59 -0300

    Set window icon on QMainWindow for taskbar (Qt6 fix)

--- main.py
+++ main.py
@@ -132,6 +132,8 @@ def main():
     from src.gui.main_window import MainWindow
 
     window = MainWindow()
+    if icon_path.exists():
+        window.setWindowIcon(QIcon(str(icon_path)))
     window.init_backend()
     window.navigate_to("start")
     window.show()
